df0c28e1e2332b4fea541c22daaa7a00a0c4023f
olya <olyakuznetsova20@gmail.com>
2024-11-28 13:13:30 +0300

    feat:  add lab1 task 2

---
+++ oop_lab_1_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+{
+    cout << "Введите длину массива n: ";
+    int n;
+    cin >> n;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1, 100);
+    cout << "Полученный массив: ";
+    int *arr = new int[n];
+    for (int i = 0; i < n; ++i)
+    {
+        arr[i] = dist(gen);
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+    // Вывод адреса начала массива
+    intptr_t base_address = reinterpret_cast<intptr_t>(&arr[0]);
+    cout << "Адрес начала массива: " << &arr[0] << endl;
+    // Вывод расстояния каждого элемента от начала массива
+    cout << "Расстояние каждого элемента от начала массива с кастами:" << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        intptr_t element_address = reinterpret_cast<intptr_t>(&arr[i]);
+        cout << "Элемент " << i << ": адрес = " << &arr[i] << ", расстояние = " << (element_address - base_address) << " байт" << endl;
+    }
+    cout << "Расстояние каждого элемента от начала массива без кастов:" << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << "Элемент " << i << ": адрес = " << &arr[i] << ", расстояние = " << (&arr[i] - &arr[0]) << " байт" << endl;
+    }
+    delete[] arr;
+    return 0;
+}
--- oop_lab_1_2.cpp
+++ oop_lab_1_2.cpp
@@ -1,38 +1,91 @@
 #include <iostream>
-#include <random>
+#include <string>
+#include <vector>
 using namespace std;
 
+// Перечисляемый тип для определения типа книги
+enum BookType
+{
+    Fiction,  // Художественная книга
+    Technical // Техническая книга
+};
+// Класс для хранения информации о книге
+class Book
+{
+public:
+    // Конструктор с параметрами
+    Book(string authorName, string bookTitle, BookType bookType)
+    {
+        // Проверяем, чтобы имя автора и название книги не были пустыми
+        if (authorName.empty() || bookTitle.empty())
+        {
+            throw invalid_argument("The author's name and book title must not be empty.");
+        }
+        author = authorName;
+        title = bookTitle;
+        type = bookType;
+    }
+    // Метод для получения типа книги
+    BookType getType()
+    {
+        return type;
+    }
+
+private:
+    string author;
+    string title;
+    BookType type;
+};
+// Класс для управления библиотекой
+class Library
+{
+private:
+    vector<Book> books; // Вектор для хранения книг
+public:
+    // Метод для добавления книги в библиотеку
+    void addBook(Book book)
+    {
+        books.push_back(book);
+    }
+    // Метод для вывода количества книг каждого типа
+    void showBookCounts()
+    {
+        int fictionCount = 0;
+        int technicalCount = 0;
+        // Проходим по всем книгам и считаем количество каждого типа
+        for (Book book : books)
+        {
+            switch (book.getType())
+            {
+            case Fiction:
+                fictionCount++;
+                break;
+            case Technical:
+                technicalCount++;
+                break;
+            }
+        }
+        // Выводим результат
+        cout << "Number of fiction books: " << fictionCount << endl;
+        cout << "Number of technical books:" << technicalCount << endl;
+    }
+};
 int main()
 {
-    cout << "Введите длину массива n: ";
-    int n;
-    cin >> n;
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dist(1, 100);
-    cout << "Полученный массив: ";
-    int *arr = new int[n];
-    for (int i = 0; i < n; ++i)
+    try
     {
-        arr[i] = dist(gen);
-        cout << arr[i] << ' ';
+        Library library; // Создаем объект библиотеки
+
+        // Добавляем книги в библиотеку
+        library.addBook(Book("Leo Tolstoy", "War and Peace", Fiction));
+        library.addBook(Book("F. M. Dostoevsky", "Idiot", Fiction));
+        library.addBook(Book("Kernighan and Ritchie", "C Programming Language", Technical));
+        // Вызываем метод для вывода количества книг
+        library.showBookCounts();
     }
-    cout << endl;
-    // Вывод адреса начала массива
-    intptr_t base_address = reinterpret_cast<intptr_t>(&arr[0]);
-    cout << "Адрес начала массива: " << &arr[0] << endl;
-    // Вывод расстояния каждого элемента от начала массива
-    cout << "Расстояние каждого элемента от начала массива с кастами:" << endl;
-    for (int i = 0; i < n; ++i)
+    catch (exception &err)
     {
-        intptr_t element_address = reinterpret_cast<intptr_t>(&arr[i]);
-        cout << "Элемент " << i << ": адрес = " << &arr[i] << ", расстояние = " << (element_address - base_address) << " байт" << endl;
+        cout << "Error: " << err.what() << endl;
     }
-    cout << "Расстояние каждого элемента от начала массива без кастов:" << endl;
-    for (int i = 0; i < n; ++i)
-    {
-        cout << "Элемент " << i << ": адрес = " << &arr[i] << ", расстояние = " << (&arr[i] - &arr[0]) << " байт" << endl;
-    }
-    delete[] arr;
     return 0;
 }
