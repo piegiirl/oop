b152df69e3fda356c05cf53ba183a7666250fb7e
piegiirl <olyakuznetsova20@gmail.com>
2024-12-19 22:10:50 +0300

    oop_lab_3_1.cpp

--- oop_lab_3_1.cpp
+++ oop_lab_3_1.cpp
@@ -1,84 +1,72 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
+#include <string>
 using namespace std;
-
-class Base {
+// Базовый класс - деталь
+class Detail
+{
 protected:
-    int value;
-
+    string name;
+    int id;
+    // конструкторы в protected
+    Detail(string n, int i) : name(n), id(i) {}
+    Detail() : name(""), id(0) {}
 public:
-    Base() : value(rand() % 100) {}
-    
-    Base(const Base& other) : value(other.value) {}
-    
-    virtual void showInfo() {
-        cout << "Base(Вывод значения): " << value << endl;
+    virtual void print() const
+    { // виртуальный метод для полиморфизма
+        cout << "Деталь: " << name << " (ID: " << id << ")" << endl;
     }
-    
-    virtual ~Base() {}
+    virtual ~Detail() {} // виртуальный деструктор
+    // шаблонная дружественная функция для создания объектов
+    template <typename T>
+    friend T *createObject(string n, int i);
 };
-
-class Derived : public Base {
+// Производный класс - сборка
+class Assembly : public Detail
+{
+protected:
+    int detailsCount;
+    Assembly(string n, int i, int count) : Detail(n, i), detailsCount(count) {}
+    Assembly() : Detail(), detailsCount(0) {}
 public:
-    Derived() : Base() {}
-    
-    Derived(const Derived& other) : Base(other) {}
-    
-    void showInfo() override {
-        cout << "Derived(вывод значения * -1): " << value * -1 << endl;
+    void print() const override
+    { // переопределение виртуального метода
+        cout << "Сборка: " << name << " (ID: " << id << "), количество деталей: "
+             << detailsCount << endl;
     }
+    template <typename T>
+    friend T *createObject(string n, int i); // та же дружественная функция
 };
-
-void add(vector<Base*>& storage, Base* obj) {
-    if (dynamic_cast<Derived*>(obj)) {
-        storage.push_back(new Derived(*dynamic_cast<Derived*>(obj)));
-    } else {
-        storage.push_back(new Base(*obj));
+// Шаблонная функция для создания объектов
+template <typename T>
+T *createObject(string n, int i)
+{
+    if constexpr (is_same_v<T, Detail>)
+    {
+        return new Detail(n, i);
     }
+    else if constexpr (is_same_v<T, Assembly>)
+    {
+        // для простоты задаем случайное количество деталей от 2 до 10
+        return new Assembly(n, i, rand() % 9 + 2);
+    }
+    return nullptr;
 }
-
-int main() {
-    srand(time(0));
-    vector<Base*> storage;
-    vector<Base*> tempObjects;
-    
-    cout << "Создание объектов:" << endl;
-    for(int i = 0; i < 10; i++) {
-        int randomNum = rand() % 100;
-        Base* obj;
-        
-        if(randomNum % 2 == 0) {
-            obj = new Base();
-            cout << "Создан Base" << endl;
-        } else {
-            obj = new Derived();
-            cout << "Создан Derived" << endl;
-        }
-        
-        tempObjects.push_back(obj);
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // для русского языка в консоли
+    srand(static_cast<unsigned>(time(0)));
+    // создаем вектор указателей на базовый класс
+    vector<Detail *> storage;
+    // создаем несколько объектов и добавляем их в вектор
+    storage.push_back(createObject<Detail>("Шпонка", 1));
+    storage.push_back(createObject<Detail>("Гребной винт", 2));
+    storage.push_back(createObject<Detail>("Коленчатый вал", 3));
+    storage.push_back(createObject<Assembly>("Двигатель", 4));
+    // выводим информацию о всех объектах
+    cout << "Содержимое хранилища:" << endl;
+    for (const auto &item : storage)
+    {
+        item->print();
     }
-    
-    for(auto obj : tempObjects) {
-        add(storage, obj);
-    }
-    
-    cout << "\nДемонстрация поведения:" << endl;
-    for(int i = 0; i < storage.size(); i++) {
-        cout << "Объект " << i + 1 << ": ";
-        storage[i]->showInfo();
-    }
-    
-    for(auto obj : tempObjects) {
-        delete obj;
-    }
-    tempObjects.clear();
-    
-    for(auto obj : storage) {
-        delete obj;
-    }
-    storage.clear();
-    
-    return 0;
 }
